2e71150f6273f6fab10228e33e4610a5c2ef1c58
Jesse Quijano <jesse@quijano.net>
2019-03-10 16:19:17 -0700

    initial commit

---
+++ Controllers/GuidController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cylance_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GuidController : ControllerBase
+    {
+        // GET api/guid
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/guid/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/guid
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/guid/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+
+        }
+
+        // DELETE api/guid/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ cylance-api.Tests/APIUnitTests.cs
@@ -0,0 +1,28 @@
+using System;
+using Xunit;
+
+namespace cylance_api.Tests
+{
+    public class WebAPIUnitTests
+    {
+
+        [Fact]
+        public void HaveDatabaseConnectivity()
+        {
+            Assert.True(false);
+        }
+
+        [Fact(Skip = "TDD: Not yet available")]
+        public void ContactAPIEndpointGenerates401()
+        {
+
+
+        }
+
+        [Fact(Skip = "TDD: Not yet available")]
+        public void ContactAPIEndpointSuccess()
+        {
+
+        }
+    }
+}
