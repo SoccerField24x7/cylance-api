3d35b3d0dac80cc7c100801e6c7e49fdfb751a5a
Jesse Quijano <jesse@quijano.net>
2019-03-24 19:19:17 -0700

    add test skeletons

--- cylance-api.Tests/APIUnitTests.cs
+++ cylance-api.Tests/APIUnitTests.cs
@@ -1,15 +1,45 @@
 using System;
+using System.Linq;
 using Xunit;
+using Microsoft.Extensions.Caching.Distributed;
+using CylanceContext.Entities;
+//using StackExchange.Redis;
 
 namespace Cylance.Api.Tests
 {
     public class WebAPIUnitTests
     {
+        private readonly IDistributedCache _cache;
+
+        public WebAPIUnitTests(IDistributedCache distributedCache)
+        {
+            _cache = distributedCache;
+        }
 
         [Fact]
-        public void HaveDatabaseConnectivity()
+        public void DatabaseIsAvailable()
         {
-            Assert.True(false);
+            bool valid;
+            try
+            {
+                using (var context = new CylanceDBContext())
+                {
+                    var result = context.Guids.FirstOrDefault();
+                }
+                valid = true;
+            }
+            catch (Exception)
+            {
+                valid = false;
+            }
+
+            Assert.True(valid);
+        }
+
+        [Fact(Skip = "TDD: Not yet available")]
+        public void EndpointsAreAvailable()
+        {
+
         }
 
         [Fact(Skip = "TDD: Not yet available")]
@@ -21,6 +51,42 @@ namespace Cylance.Api.Tests
 
         [Fact(Skip = "TDD: Not yet available")]
         public void ContactAPIEndpointSuccess()
+        {
+
+        }
+
+        [Fact(Skip = "TDD: Not yet available")]
+        public void CreateGuidSuccess()
+        {
+
+        }
+
+        [Fact(Skip = "TDD: Not yet available")]
+        public void CreateGuidFail()
+        {
+
+        }
+
+        [Fact(Skip = "TDD: Not yet available")]
+        public void PatchGuidSuccess()
+        {
+
+        }
+
+        [Fact(Skip = "TDD: Not yet available")]
+        public void PatchGuidFailsValidation()
+        {
+
+        }
+
+        [Fact(Skip = "TDD: Not yet available")]
+        public void DeleteGuidSuccess()
+        {
+
+        }
+
+        [Fact(Skip = "TDD: Not yet available")]
+        public void DeleteGuidIsIdempotent()
         {
 
         }
