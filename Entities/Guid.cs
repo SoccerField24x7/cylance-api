6cf098fe939753dc1abcd54754b423499bb81f77
Jesse Quijano <jesse@quijano.net>
2019-04-03 12:47:00 -0700

    Update the namespace and change key to be UUID

--- Entities/Guid.cs
+++ Entities/Guid.cs
@@ -1,13 +1,13 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace PGContext.Entities
+namespace CylanceContext.Entities
 {
     [Table("Guid", Schema = "public")]
     public class Guid
     {
         [Key]
-        public int Id { get; set; }
+        public System.Guid Id { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
     }
