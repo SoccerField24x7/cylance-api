e3280f39b8451ccc7ca22429700818b111d66e14
Jesse Quijano <jesse@quijano.net>
2019-04-03 12:56:50 -0700

    Add db context

---
+++ Entities/CylanceContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+using System.IO;
+
+namespace CylanceContext.Entities
+{
+    public class CylanceDBContext : DbContext
+    {
+        public CylanceDBContext() : base() {}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var conn = ConfigurationManager.ConnectionStrings["PostgreSQL"];
+            if (conn == null) // need to manually find the config for Unit Tests 
+            {
+                var dir = Directory.GetCurrentDirectory();
+                var strings = ConfigurationManager.OpenExeConfiguration($"{dir}/cylance-api.dll").ConnectionStrings;
+                conn = strings.ConnectionStrings["PostgreSQL"];
+            }
+            optionsBuilder.UseNpgsql(conn.ToString());
+        }
+
+        public DbSet<Guid> Guids { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Cylance Code Challenge ReadMe
+
+## Design Decisions
+* Less design, but for a challenge I decided to do entire project on Visual Studio for Mac.
+* App.config - there are a lot of places to store credentials. Ideally, a credential store that is not commited in the code, that allows for injection based on environment.
